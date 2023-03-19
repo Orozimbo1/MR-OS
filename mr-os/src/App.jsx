caf0da717168aab63f1adf74a54acab409a2ecdc
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-19 18:07:05 -0300

    Criando hook de autenticação e limitando as rotas

--- mr-os/src/App.jsx
+++ mr-os/src/App.jsx
@@ -9,17 +9,26 @@ import { Navbar, Footer } from './components'
 // Pages
 import { Home, ServiceOrder, Dashboard, Login, Register } from './pages'
 
+// Hooks
+import { useAuth } from './hooks'
+
 function App() {
+  const { auth, loading } = useAuth()
+
+  if(loading) {
+    return <p>Carregando ...</p>
+  }
+
   return (
     <BrowserRouter>
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path='/' element={<Home />} /> 
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/order-service' element={<ServiceOrder />} /> 
-          <Route path='/login' element={<Login />} /> 
-          <Route path='/register' element={<Register />} /> 
+          <Route path='/' element={auth ? <Home /> : <Navigate to='/login' />} /> 
+          <Route path='/dashboard' element={auth ? <Dashboard /> : <Navigate to='/login' />} />
+          <Route path='/order-service' element={auth ? <ServiceOrder /> : <Navigate to='/login' />} /> 
+          <Route path='/login' element={!auth ? <Login /> : <Navigate to='/' />} /> 
+          <Route path='/register' element={!auth ? <Register /> : <Navigate to='/' />} /> 
         </Routes>
       </div>
       <Footer />
