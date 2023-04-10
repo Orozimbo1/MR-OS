131b311a120f37fd37f04b70a843457c5aeb80e1
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-10 13:21:24 -0300

    Criando função de fechar o menu lateral

--- mr-os/src/components/SideMenu/SideMenu.jsx
+++ mr-os/src/components/SideMenu/SideMenu.jsx
@@ -1,12 +1,18 @@
 import './SideMenu.css'
-import React from 'react'
+import { useRef } from 'react'
 
 import { Link } from 'react-router-dom'
 
+// Context
+import { useStateContext } from '../../context/StateContext'
+
 const SideMenu = ({ array, direction }) => {
+  const { setShowMenu } = useStateContext()
+  const elementref = useRef()
+
   return (
     <div>
-      <div className='blackout'></div>
+      <div className='blackout' onClick={() => setShowMenu(false)}></div>
       <div className={`side-menu ${direction}`}>
         {array && array.map((item, i) => (
           <ul key={i}>
