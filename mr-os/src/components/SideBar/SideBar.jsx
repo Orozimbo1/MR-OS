227c434d7654273f09d3b5230d435ac053f92a99
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-04-17 21:43:02 -0300

    sidemenu

--- mr-os/src/components/SideBar/SideBar.jsx
+++ mr-os/src/components/SideBar/SideBar.jsx
@@ -2,6 +2,12 @@ import './SideBar.css'
 
 // Icons
 import { BsStar } from 'react-icons/bs'
+import { AiOutlineDashboard } from 'react-icons/ai'
+import { AiOutlineSetting } from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
+import { MdOutlineContactSupport } from 'react-icons/md'
+import { BsCashStack } from 'react-icons/bs'
+import { BsCardChecklist } from 'react-icons/bs'
 
 // Context
 import { useStateContext } from '../../context/StateContext'
@@ -25,16 +31,12 @@ const SideBar = () => {
         <BsStar />
       </div>
       <ul>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
+        <li><AiOutlineDashboard size={30}/></li>
+        <li><BsCashStack size={30} /></li>
+        <li><BsCardChecklist size={30} /></li>
+        <li><AiOutlineSetting size={30}/></li>
+        <li><MdOutlineContactSupport size={30}/></li>
+        <li><AiFillGithub size={30}/></li>
       </ul>
     </div>
   )
