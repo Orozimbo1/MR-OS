df900a8824d56cc4b7a1bf26f03f2504f867874b
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-05-06 21:52:40 -0300

    Ajustes no CSS

--- mr-os/src/components/CardOrderService/CardOrderService.jsx
+++ mr-os/src/components/CardOrderService/CardOrderService.jsx
@@ -7,21 +7,23 @@ const CardOrderService = ({ name, address, phoneNumber, createdAt, id, devices, 
   const date = createdAt && createdAt.toDate() && createdAt.toDate().toLocaleString('pt-BR', { timezone: 'UTC' })
 
   return (
-    <div className='card-order-service'>
+    <div className='card-order-service-container'>
       <Link to={`/order-service/${id}`}>
-        <div className={`qtd ${status}`}>{devices.length}</div>
-        <div>
-          <h3>{name}</h3>
-          <p>{address}</p>
-        </div>
-        <div>
-          <p>{phoneNumber}</p>
-          <p className='date'>{date}</p>
-        </div>
-        <div className="total">
-            <h4>Total</h4>
-            <p><span>R$:</span> {devices.reduce((acc, val) => acc + val.total, 0)}</p>
+        <div className='card-order-service'>
+          <div className={`qtd ${status}`}>{devices.length}</div>
+          <div>
+            <h3>{name}</h3>
+            <p>{address}</p>
           </div>
+          <div>
+            <p>{phoneNumber}</p>
+            <p className='date'>{date}</p>
+          </div>
+          <div className="total">
+              <h4>Total</h4>
+              <p><span>R$:</span> {devices.reduce((acc, val) => acc + val.total, 0)}</p>
+            </div>
+        </div>
       </Link>
     </div>
   )
