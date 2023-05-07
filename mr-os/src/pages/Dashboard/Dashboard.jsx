e09c907e7dd20fdbbf8b87dc6760db6c21b6dfeb
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-05-07 19:45:03 -0300

    Esboço da pagina d Dashboard

--- mr-os/src/pages/Dashboard/Dashboard.jsx
+++ mr-os/src/pages/Dashboard/Dashboard.jsx
@@ -6,13 +6,50 @@ const Dashboard = () => {
 
   return (
     <div className='dashboard'>
-      <div id='dash'>
-        <h1>Dashboard</h1>
-        <Link to='#finances'>finanças</Link>
-      </div>
-      <div id='finances'>
-        <h1>Finanças</h1>
-      </div>
+      <main className='dash'>
+        <section className='topics'>
+          <h2>Dashboard</h2>
+          <div className='dash-data'>
+            <div className='dash-card'>
+              <h4>Em andamento</h4>
+              <p>10</p>
+            </div>
+            <div className='dash-card'>
+              <h4>Rejeitadas</h4>
+              <p>20</p>
+            </div>
+            <div className='dash-card'>
+              <h4>Concluídas</h4>
+              <p>30</p>
+            </div>
+          </div>
+        </section>
+        <section className='topics'>
+          <h2>Finanças</h2>
+          <div className="dash-data">
+            <div className='dash-card'>
+              <h4>Total</h4>
+              <p>R$:200</p>
+            </div>
+            <div className='dash-card'>
+              <h4>Gastos</h4>
+              <p>R$:50</p>
+            </div>
+            <div className='dash-card'>
+              <h4>Lucro</h4>
+              <p>R$:150</p>
+            </div>
+          </div>
+        </section>
+      </main>
+      <aside className='dash-graphic'>
+        <div className='graphic'>
+          <p></p>
+        </div>
+        <div className='graphic'>
+          <p></p>
+        </div>
+      </aside>
     </div>
   )
 }
