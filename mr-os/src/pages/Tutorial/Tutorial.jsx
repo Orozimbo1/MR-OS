60f34fcd88c92afca223e9affbd3060cdd8d953b
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-05-12 22:49:59 -0300

    JSX da página de Tutorial

--- mr-os/src/pages/Tutorial/Tutorial.jsx
+++ mr-os/src/pages/Tutorial/Tutorial.jsx
@@ -2,7 +2,20 @@ import './Tutorial.css'
 
 const Tutorial = () => {
   return (
-    <div>Tutorial</div>
+    <main>
+      <section>
+        <h2>O que é MR Os</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae assumenda nemo reiciendis veniam. Illo nam atque nemo earum dolor necessitatibus at possimus voluptas dignissimos? Ullam optio perferendis suscipit tenetur cumque.</p>
+      </section>
+      <section>
+        <h2>Como criar uma ordem de serviço?</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae assumenda nemo reiciendis veniam. Illo nam atque nemo earum dolor necessitatibus at possimus voluptas dignissimos? Ullam optio perferendis suscipit tenetur cumque.</p>
+      </section>
+      <section>
+        <h2>Todas funções de MR Os</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae assumenda nemo reiciendis veniam. Illo nam atque nemo earum dolor necessitatibus at possimus voluptas dignissimos? Ullam optio perferendis suscipit tenetur cumque.</p>
+      </section>
+    </main>
   )
 }
 
