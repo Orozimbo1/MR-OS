dd1fdebe2300713320870178238e06d0583acc91
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-05-12 22:52:45 -0300

    Esboo da pagina de settings

--- mr-os/src/pages/Settings/Settings.jsx
+++ mr-os/src/pages/Settings/Settings.jsx
@@ -2,7 +2,18 @@ import './Settings.css'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <main>
+      <section>
+        <h3>Meus dados</h3>
+        <p>Nome da empresa</p>
+        <p>email</p>
+        <ul>
+          <li>Redefinir senha</li>
+          <li>Mudar nome da empresa</li>
+          <li>Excluiir conta</li>
+        </ul>
+      </section>
+    </main>
   )
 }
 
