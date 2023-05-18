e397b7a9944c595a5862de902628dc83ebe4ba12
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-05-18 20:33:34 -0300

    JSX da página de configurações

--- mr-os/src/pages/Settings/Settings.jsx
+++ mr-os/src/pages/Settings/Settings.jsx
@@ -1,18 +1,47 @@
+import { BsExclamationTriangle } from 'react-icons/bs'
+import { MdPassword } from 'react-icons/md'
+import { SlUserUnfollow } from 'react-icons/sl'
 import './Settings.css'
 
 const Settings = () => {
   return (
     <main>
-      <section>
-        <h3>Meus dados</h3>
-        <p>Nome da empresa</p>
-        <p>email</p>
-        <ul>
-          <li>Redefinir senha</li>
-          <li>Mudar nome da empresa</li>
-          <li>Excluiir conta</li>
-        </ul>
-      </section>
+      <h2>Meus dados</h2>
+      <form>
+        <label>
+          <span>Razão social</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span>Email</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span>Endereço</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span>CNPJ ou CPF</span>
+          <input type="text" />
+        </label>
+        <div className='danger-zone'>
+          <span><BsExclamationTriangle /> Danger zone</span>
+          <button type='button' onClick=''>
+              <SlUserUnfollow />
+              Excluir conta
+          </button>
+          <button type='button' onClick=''>
+              <MdPassword />
+              Redefinir senha
+          </button>
+        </div>
+        <div className='finish-or-cancel'>
+            <button type='button' className='cancel-btn' to='/'>Cancelar</button>
+            <input type="submit" value="Atualizar" />
+            {/* {!loading && <input type="submit" value="Finalizar" />}
+            {loading && <input type="submit" value="Aguarde.." disabled />} */}
+          </div>
+      </form>
     </main>
   )
 }
