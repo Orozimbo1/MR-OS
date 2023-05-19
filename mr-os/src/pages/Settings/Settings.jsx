3ccb59aed0056da5b07cffec380978132858bbf4
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-05-18 21:09:31 -0300

    Consumindo dados do redux

--- mr-os/src/pages/Settings/Settings.jsx
+++ mr-os/src/pages/Settings/Settings.jsx
@@ -1,46 +1,74 @@
+import './Settings.css'
+
+// Icons
 import { BsExclamationTriangle } from 'react-icons/bs'
 import { MdPassword } from 'react-icons/md'
 import { SlUserUnfollow } from 'react-icons/sl'
-import './Settings.css'
+
+// Hooks
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
 
 const Settings = () => {
+  const { user, loading } = useSelector((state) => state.auth)
+
+  const [displayName, setDisplayName] = useState(user.displayName || '')
+  const [email, setEmail] = useState(user.email || '')
+  const [address, setAddress] = useState(user.address || '')
+  const [CNPJ, setCNPJ] = useState(user.CNPJ || '')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <main>
       <h2>Meus dados</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Razão social</span>
-          <input type="text" />
+          <input type="text" 
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
         </label>
         <label>
           <span>Email</span>
-          <input type="text" />
+          <input type="text" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label>
           <span>Endereço</span>
-          <input type="text" />
+          <input type="text" 
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
         </label>
         <label>
-          <span>CNPJ ou CPF</span>
-          <input type="text" />
+          <span>CNPJ</span>
+          <input type="text" 
+            value={CNPJ}
+            onChange={(e) => setCNPJ(e.target.value)}
+          />
         </label>
         <div className='danger-zone'>
           <span><BsExclamationTriangle /> Danger zone</span>
-          <button type='button' onClick=''>
+          <button type='button' onClick={() => alert('Estamos trabalhando nisso. ;)')}>
               <SlUserUnfollow />
               Excluir conta
           </button>
-          <button type='button' onClick=''>
+          <button type='button' onClick={() => alert('Estamos trabalhando nisso. ;)')}>
               <MdPassword />
               Redefinir senha
           </button>
         </div>
         <div className='finish-or-cancel'>
-            <button type='button' className='cancel-btn' to='/'>Cancelar</button>
-            <input type="submit" value="Atualizar" />
-            {/* {!loading && <input type="submit" value="Finalizar" />}
-            {loading && <input type="submit" value="Aguarde.." disabled />} */}
-          </div>
+          <button type='button' className='cancel-btn'>Cancelar</button>
+          {!loading && <input type="submit" value="Atualizar" />}
+          {loading && <input type="submit" value="Aguarde.." disabled />}
+        </div>
       </form>
     </main>
   )
