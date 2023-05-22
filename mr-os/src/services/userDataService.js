8c547d3d8de684698f65ed83db2b16a50204f9e5
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-05-21 22:18:34 -0300

    Criando o userData.js e a função de inserção de dados do usuário

---
+++ mr-os/src/services/userDataService.js
@@ -0,0 +1,27 @@
+import { db } from "../firebase/config";
+import { collection, addDoc, Timestamp, getDocs, getDoc, query, where, orderBy, doc, updateDoc } from "firebase/firestore";
+
+// Register user data
+const registerUserData = async (document) => {
+
+  try {
+
+    const newDocument = { ...document }
+    const addDocument = await addDoc(
+      collection(db, 'userData'),
+      newDocument
+    )
+
+    return newDocument
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+
+const userData = {
+  registerUserData
+}
+
+export default userData;
