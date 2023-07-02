d45740c83a74eff7e702792849c813536fd0ec0d
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-07-01 23:08:06 -0300

    Tranformando o CSS do componente MEssage em modulo

--- src/components/Messages/Message.jsx
+++ src/components/Messages/Message.jsx
@@ -1,8 +1,8 @@
-import './Message.css'
+import styles from './Message.module.css'
 
 const Message = ({msg, type}) => {
   return (
-    <div className={`message ${type}`}>
+    <div className={`${styles.message} ${type == 'error' ? styles.error : styles.success}`}>
       <h5>{msg}</h5>
     </div>
   )
