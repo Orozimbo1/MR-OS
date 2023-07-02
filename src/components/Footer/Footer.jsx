4a2b0c54e7173d55e4626b25a726dac4a76bd2e1
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-07-01 23:03:53 -0300

    Transformando o CSS do Footer em modulo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
-import './Footer.css'
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer id='footer'>
+    <footer id={styles.footer}>
       <h4>MR'sOs &copy; 2023</h4>
       <h3>Powered by Hashtech</h3>
     </footer>
