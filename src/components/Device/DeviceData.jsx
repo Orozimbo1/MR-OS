163eb0fadb0124db819677fe33f500cfec0f6ad5
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-07-01 23:02:19 -0300

    Transformando o CSS do componente de dispositivos em modulo

--- src/components/Device/DeviceData.jsx
+++ src/components/Device/DeviceData.jsx
@@ -1,4 +1,4 @@
-import './DeviceData.css'
+import styles from './DeviceData.module.css'
 
 // Icons
 import { BsTrash, BsPenFill } from 'react-icons/bs'
@@ -17,23 +17,23 @@ function DeviceData({ showActions, handleDelete, device, setDevice }) {
   }
 
   return (
-      <div id='data'>
+      <div id={styles.data}>
         {showActions && (
           <>
-            <BsTrash className='trash' onClick={handleDelete}/>
+            <BsTrash className={styles.trash} onClick={handleDelete}/>
             <BsPenFill onClick={editDevice} />
           </>
         )}
-        <div className="tec-data">
+        <div className={styles.tec_data}>
           <p><span>Tipo do dispositvo:</span> {deviceType}</p>
           <p><span>Marca:</span> {brand}</p>
           <p><span>Modelo:</span> {model}</p>
           <p><span>Cor:</span> {color}</p>
-          <div className='problem-desc'>
+          <div className={styles.problem_desc}>
             <span>Descrição do problema:</span>
             <p>{problemDesc}</p>
           </div>
-          <div className='subtotal'>
+          <div className={styles.subtotal}>
             <span>Subtotal</span>
             <p><strong>R$: </strong>{total}</p>
           </div>
