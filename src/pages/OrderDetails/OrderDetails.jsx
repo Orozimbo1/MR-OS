dc14efa1f1bfb01b5ece2a2fadf66029abe6a4d4
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-07-06 23:41:28 -0300

    Ajustes

--- src/pages/OrderDetails/OrderDetails.jsx
+++ src/pages/OrderDetails/OrderDetails.jsx
@@ -41,7 +41,7 @@ const Order = () => {
       {order && order.status && (
         <div>
           <div className={styles.customer_data}>
-            <h2>Nome: {order.name}</h2>
+            <h2>{order.name}</h2>
             <h3>Endereço: {order.address}</h3>
             <h3>Telefone: {order.phoneNumber}</h3>
             <h3>Data de criação: {date}</h3>
