87242e9bf61933129d675e370e4c37ae33dae5de
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-07-31 22:56:22 -0300

    .

--- src/components/Print/Print.jsx
+++ src/components/Print/Print.jsx
@@ -1,8 +1,30 @@
 import styles from './Print.module.css'
+import logo from '../../assets/logo_completa.png'
+
+// Context
+import { useStateContext } from '../../context/StateContext'
 
 const Print = () => {
+  const { setShowPrint } = useStateContext()
+
   return (
-    <div>Print</div>
+    <div className={styles.lightblue} onClick={() => setShowPrint(false)}>
+      <div className={styles.print_order}>
+        <hr />
+        <header className={styles.header}>
+          <section className={styles.logo}>
+            <img src={logo} alt="Logo da empresa" />
+          </section>
+          <section className={styles.data}>
+            <h1>SSL Assistencia Tecnica Celulares e Informatica</h1>
+          </section>
+          <section className={styles.id}>
+            <p>ola</p>
+          </section>
+        </header>
+        <hr />
+      </div>
+    </div>
   )
 }
 
