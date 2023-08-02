9f1773b676f7db0887bc688be327289a8629f13f
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-08-01 21:53:02 -0300

    HTML Do Print

--- src/components/Print/Print.jsx
+++ src/components/Print/Print.jsx
@@ -17,11 +17,79 @@ const Print = () => {
           </section>
           <section className={styles.data}>
             <h1>SSL Assistencia Tecnica Celulares e Informatica</h1>
+            <h3>CNPJ</h3>
+            <p>Endereço</p>
+            <p>Email</p>
+            <p>Responsável</p>
           </section>
           <section className={styles.id}>
-            <p>ola</p>
+            <h3>Identificação da order</h3>
+            <p>Data de criação</p>
           </section>
         </header>
+        <main>
+          <section>
+            <h2>Cliente</h2>
+            <p>Nome do Cliente</p>
+            <p>Endereço do cliente</p>
+            <p>Email</p>
+            <p>Celular do cliente</p>
+          </section>
+          <hr />
+          <section>
+            <h3>Status da OS: <p>Orçamento</p></h3>
+            <h3>Data inicial: <p>10/10/2020</p></h3>
+            <h3>Data final: <p>10/10/2020</p></h3>
+            <h3>Garantia: <p>90 dias</p></h3>
+          </section>
+          <hr />
+          {/* Loop de dispositivos */}
+          <section>
+            <h3>Dispositivo: <p>Tipo de dispositivo + Marca + Modelo</p></h3>
+            <hr />
+            <h3>Defeito apresentado: <p>Descrição do problema</p></h3>
+            <hr />
+            <h3>Laudo técnico: <p>Laudo técnico</p></h3>
+            <hr />
+            {/* Condicional de peças */}
+            <table>
+              <tr>
+                <td>Produtos</td>
+                <td>Qtd</td>
+                <td>V. UN R$</td>
+                <td>S. Total R$</td>
+              </tr>
+              {/* Loop de peças */}
+              <tr>
+                <td>Peça</td>
+                <td>Quantidade de peças</td>
+                <td>Valor da peça</td>
+                <td>Valor Total</td>
+              </tr>
+              {/* Final de loop */}
+              <tr>
+                <td>Total</td>
+                <td>R$ Valor total</td>
+              </tr>
+            </table>
+          </section>
+          <h3>Valor Total da OS: R$:000</h3>
+        </main>
+        <hr />
+        <footer>
+          <table>
+            <tr>
+              <td>Data</td>
+              <td>Assinatura do Cliente</td>
+              <td>Assinatura do Tecnico Responsável</td>
+            </tr>
+            <tr>
+              <td> . </td>
+              <td> . </td>
+              <td> . </td>
+            </tr>
+          </table>
+        </footer>
         <hr />
       </div>
     </div>
